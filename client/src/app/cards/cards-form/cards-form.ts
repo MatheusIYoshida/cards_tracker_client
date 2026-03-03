01c531fd8eb29b1fd2b77b5ac47bd3cd364f141e
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-03-02 21:43:45 -0300

    fix (CardsForm): Substitute textarea component

--- client/src/app/cards/cards-form/cards-form.ts
+++ client/src/app/cards/cards-form/cards-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormInput } from '../../shared/ui/form-input/form-input';
 import { FormButton } from '../../shared/ui/form-button/form-button';
+import { Textarea } from '../../shared/ui/textarea/textarea';
 
 @Component({
   selector: 'app-cards-form',
   standalone: true,
-  imports: [FormInput, FormButton],
+  imports: [FormInput, FormButton, Textarea],
   templateUrl: './cards-form.html',
   styleUrl: './cards-form.scss',
 })
