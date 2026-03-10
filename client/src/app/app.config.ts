4cb7892f9383809e35b34a47efae33b6253d4a1e
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-03-09 21:40:00 -0300

    feat: add ngx-translate

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
+export function HttpLoaderFactory() {
+  return new TranslateHttpLoader();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateHttpLoader(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'pt-br',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]  
+        }
+      })
+    )
   ]
 };
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('lang') || 'pt-br';
+    translate.use(savedLang);
+  }
+
+  ngOnInit() {
+    localStorage.setItem('lang', 'pt-br');
+  }
 }
