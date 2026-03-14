76f9d0cc2bd534931508eb9a555ee1b4414abfe7
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-03-14 10:50:05 -0300

    feat (cards-list): add mat-icon to button

--- client/src/app/cards/cards-list/cards-list.ts
+++ client/src/app/cards/cards-list/cards-list.ts
@@ -1,10 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
 import { CardsForm } from '../cards-form/cards-form';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cards-list',
   standalone: true,
-  imports: [CardsForm],
+  imports: [
+    CardsForm,
+    MatIcon
+  ],
   templateUrl: './cards-list.html',
   styleUrl: './cards-list.scss',
 })
