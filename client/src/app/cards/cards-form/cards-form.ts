1f2a6639dfe2ce29eecd014b481f5525e4170a5d
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-03-17 21:24:25 -0300

    feat (cardsForm/cardsList): add translation to components

--- client/src/app/cards/cards-form/cards-form.ts
+++ client/src/app/cards/cards-form/cards-form.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FormInput } from '../../shared/ui/form-input/form-input';
 import { FormButton } from '../../shared/ui/form-button/form-button';
 import { Textarea } from '../../shared/ui/textarea/textarea';
+import { MatIcon } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cards-form',
   standalone: true,
-  imports: [CommonModule, FormInput, FormButton, Textarea],
+  imports: [
+    CommonModule, 
+    FormInput, 
+    FormButton, 
+    Textarea,
+    MatIcon,
+    TranslateModule
+  ],
   templateUrl: './cards-form.html',
   styleUrl: './cards-form.scss',
 })
--- client/src/app/cards/cards-list/cards-list.ts
+++ client/src/app/cards/cards-list/cards-list.ts
@@ -1,13 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
 import { CardsForm } from '../cards-form/cards-form';
 import { MatIcon } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cards-list',
   standalone: true,
   imports: [
     CardsForm,
-    MatIcon
+    MatIcon,
+    TranslateModule
   ],
   templateUrl: './cards-list.html',
   styleUrl: './cards-list.scss',
