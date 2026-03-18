854f7535b17cf777b2c5ca2cdec002d527129876
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-03-17 21:54:52 -0300

    fix(textarea): add translation module to imports

--- client/src/app/shared/ui/textarea/textarea.ts
+++ client/src/app/shared/ui/textarea/textarea.ts
@@ -2,11 +2,17 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-textarea',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    TranslateModule
+  ],
   templateUrl: './textarea.html',
   styleUrls: ['./textarea.scss'],
 })
